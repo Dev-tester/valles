7cbb3f0be813dd9e3103b9a4e60734805077f76d
Evgeny Sobolev <dev@mail.com>
2020-06-22 01:08:41 +0400

    Сделан первичный каркас Приложения для демонстрации

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -2,15 +2,82 @@
 
 namespace app\controllers;
 
-class MainController extends \yii\web\Controller
-{
-    public function actionIndex()
-    {
-        $hello = 'Hello World';
-        return $this->render('index',
-            [
-                'hello' => $hello
-            ]);
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\data\ArrayDataProvider;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\Product;
+use app\models\LoginForm;
+use app\models\UploadedFile as Upload;
+use yii\web\UploadedFile;
+
+class MainController extends \yii\web\Controller {
+    public function actionIndex(){
+	    // проверяем авторизацию
+    	if (Yii::$app->user->isGuest){                                                                                  // если не авторизован, авторизация
+		    $model = new LoginForm();
+		    if ($model->load(Yii::$app->request->post()) && $model->login()) {
+			    return $this->goBack();
+		    }
+
+		    $model->password = '';
+		    return $this->render('login', [
+			    'model' => $model,
+		    ]);
+	    }
+	    // показываем форму
+	    $model = new Upload();
+	    $productProvider = new ActiveDataProvider([
+		    'query' => Product::find()->where(['AND','buy_price>100','buy_price<200'])->orderBy(['title' => SORT_ASC]),
+		    'pagination' => [
+			    'pageSize' => 20,
+		    ],
+	    ]);
+
+	    return $this->render('index', [
+		        'model' => $model,
+		        'productProvider' => $productProvider,
+		        'uploadsProvider' => null
+	    ]);
     }
 
+	public function actionUpload(){
+    	$model = new Upload();
+		$uploadsProvider = null;
+		if (Yii::$app->request->isPost){
+			$model->priceFile = UploadedFile::getInstance($model, 'priceFile');
+			if ($model->upload()){
+				$uploadsProvider = new ArrayDataProvider([
+					'allModels' => $model->result,
+					'pagination' => [
+						'pageSize' => 10,
+					],
+				]);
+			}
+		}
+		$productProvider = new ActiveDataProvider([
+			'query' => Product::find()->where(['AND','buy_price>100','buy_price<200'])->orderBy(['title' => SORT_ASC]),
+			'pagination' => [
+				'pageSize' => 20,
+			],
+		]);
+		return $this->render('index', [
+				'model' => $model,
+				'productProvider' => $productProvider,
+				'uploadsProvider' => $uploadsProvider
+		]);
+	}
+
+	/**
+	 * Logout action.
+	 *
+	 * @return Response
+	 */
+	public function actionLogout(){
+		Yii::$app->user->logout();
+		return $this->goHome();
+	}
 }
--- views/main/index.php
+++ views/main/index.php
@@ -1,9 +1,77 @@
 <?php
 /* @var $this yii\web\View */
-/*  @var $hello string */
-?>
-<h1>Это main/index</h1>
+/* @var $productProvider ActiveDataProvider */
+/* @var $uploadsProvider ArrayDataProvider */
 
-<p>
-<?= $hello ?>
-</p>
+use yii\widgets\ActiveForm;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+use yii\data\ArrayDataProvider;
+use yii\bootstrap\Modal;
+use yii\helpers\Html;
+
+$uploaded = !empty($model->result);
+?>
+<div class="container-all">
+	<div class="container-top<?= $uploaded ? ' uploaded':''?>">
+		<?php
+		if ($uploaded){
+			echo GridView::widget([
+				'dataProvider' => $uploadsProvider,
+			]);
+		}
+		?>
+	</div>
+	<div class="container-middle<?= $uploaded ? ' uploaded':''?>">
+		<!--<h1>Это main/index</h1>
+		<p></p>-->
+		<?php
+		if ($uploaded){
+		?>
+			<strong>Выберите из списка поле из которого будет обновлена Закупочная цена:</strong>
+		<?php
+			$form = ActiveForm::begin();
+			$columns = array_keys($model->result[0]);
+			echo Html::dropDownList('buy_price', null, $columns);
+			ActiveForm::end();
+			?>
+			<button class="btn btn-primary">Обновить</button>
+			<?php
+		}
+		else{
+			Modal::begin([
+				'header' => '<b>Выберите файл прайса Поставщика</b>',
+				'toggleButton' => [
+					'label' => 'Обновить цены',
+					'tag' => 'button',
+					'class' => 'btn btn-primary',
+				],
+				'footer' => '&nbsp;',
+			]);
+				$form = ActiveForm::begin([
+					'options' => [
+						'enctype' => 'multipart/form-data'
+					],
+					'action'  => ['/main/upload']
+				]);
+				?>
+				<?= $form->field($model,'priceFile')->fileInput([
+					'accept'    => 'application/pdf,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel,application/msword,image/*',
+				])
+				?>
+				<span class="comment">(допустимые форматы pdf, xls, xlsx, doc, docx)</span>
+				<button class="btn btn-primary">Загрузить</button>
+				<?php ActiveForm::end();
+			Modal::end();
+		}
+		?>
+	</div>
+	<div><h4>Прайс Валлес</h4></div>
+	<div class="container-bottom<?= $uploaded ? ' uploaded':''?>">
+		<?php
+		echo GridView::widget([
+			'dataProvider' => $productProvider,
+		]);
+		?>
+	</div>
+</div>
